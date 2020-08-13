cb1b4a62a2ddbe61081d20613526195b06a68e00
GageM <dgmille2@ncsu.edu>
2020-08-12 23:44:55 -0400

    GPIO Test on event

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ gpio.on('change', function(channel, value) {
 	console.log('Channel ' + channel + ' value is now ' + value);
 });
 gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+gpio.setup(7, gpio.DIR_OUT, write);
+
+function write(err) {
+    if (err) throw err;
+    gpio.write(7, true, function(err) {
+        if (err) throw err;
+        console.log('Written to pin');
+    });
+}
