13d6be2bbc7752ab2cab6c61970008850ce93ea6
GageM <dgmille2@ncsu.edu>
2020-08-12 23:09:23 -0400

    New GPIO library test

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+var gpio = require('../rpi-gpio');
 
-function blinkLED() { //function to start blinking
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
-}
-
-function endBlink() { //function to stop blinking
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport GPIO to free resources
-}
-
-setTimeout(endBlink, 10000); //stop blinking after 5 seconds
+gpio.on('change', function(channel, value) {
+	console.log('Channel ' + channel + ' value is now ' + value);
+});
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
---
+++ motorController.js
@@ -0,0 +1,13 @@
+var resolution = {
+    'Full': [0, 0, 0],
+    'Half': [1, 0, 0],
+    '1/4': [0, 1, 0],
+    '1/8': [1, 1, 0],
+    '1/16': [1, 1, 1]
+}
+
+
+
+const moveMotor = (steps, direction, delay, stepType) => {
+    
+}
