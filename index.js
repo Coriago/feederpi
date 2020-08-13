09ea603aed346ccefc80639fca8253e2996262cc
GageM <dgmille2@ncsu.edu>
2020-08-12 23:49:28 -0400

    Test3

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var gpio = require('rpi-gpio');
 gpio.on('change', function(channel, value) {
 	console.log('Channel ' + channel + ' value is now ' + value);
 });
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 function write(err) {
     if (err) throw err;
@@ -13,4 +12,4 @@ function write(err) {
     });
 }
 
-gpio.setup(7, gpio.DIR_OUT, write);
+gpio.setup(7, gpio.DIR_OUT, gpio.EDGE_BOTH, write);
