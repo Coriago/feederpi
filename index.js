5b84808a45115eff73117f7376e69a87bdc23568
GageM <dgmille2@ncsu.edu>
2020-08-13 22:38:21 -0400

    Async

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
-
-function blinkLED() { //function to start blinking
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
+const led = new Gpio(4, 'out');       // Export GPIO17 as an output
+let stopBlinking = false;
+ 
+// Toggle the state of the LED connected to GPIO17 every 200ms
+const blinkLed = _ => {
+  if (stopBlinking) {
+    return led.unexport();
   }
-}
-
-function endBlink() { //function to stop blinking
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport GPIO to free resources
-}
-
-setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+ 
+  led.read()
+    .then(value => led.write(value ^ 1))
+    .then(_ => setTimeout(blinkLed, 200))
+    .catch(err => console.log(err));
+};
+ 
+blinkLed();
+ 
+// Stop blinking the LED after 5 seconds
+setTimeout(_ => stopBlinking = true, 5000);
