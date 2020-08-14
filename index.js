f4adddcb9f6302b5a8c24bb5646382e62fc889fe
GageM <dgmille2@ncsu.edu>
2020-08-13 21:10:10 -0400

    Changed Pins

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { MotorCotroller } = require('./motorController');
 const MotorController = require('./motorController');
 
-const step_pin = 0;
-const direction_pin = 0;
-const ms1_pin = 0;
-const ms2_pin = 0;
-const ms3_pin = 0;
+const step_pin = 18;
+const direction_pin = 16;
+const ms1_pin = 36;
+const ms2_pin = 38;
+const ms3_pin = 40;
 
 const motor = new MotorController(step_pin, direction_pin, ms1_pin, ms2_pin, ms3_pin);
 
