2dd794635370b50cdf06baedb8f4ae968272e9e4
GageM <dgmille2@ncsu.edu>
2020-08-13 21:16:43 -0400

    Blinking light test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MotorCotroller } = require('./motorController');
+const gpio = require('rpi-gpio').promise;
 const MotorController = require('./motorController');
 
 const step_pin = 18;
@@ -7,6 +8,15 @@ const ms1_pin = 36;
 const ms2_pin = 38;
 const ms3_pin = 40;
 
-const motor = new MotorController(step_pin, direction_pin, ms1_pin, ms2_pin, ms3_pin);
+//const motor = new MotorController(step_pin, direction_pin, ms1_pin, ms2_pin, ms3_pin);
 
-motor.moveMotor(180, true, 1000);
+//motor.moveMotor(180, true, 1000);
+
+const sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+gpio.setup(this.step_pin).then(() => {
+  gpio.write(this.step_pin, true).then(() => sleep(delay));
+  gpio.write(this.step_pin, false).then(() => sleep(delay));
+});
