d46ff361231df3f841c568847f2b6b75d7827080
GageM <dgmille2@ncsu.edu>
2020-08-13 22:26:54 -0400

    change to another blinky

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
-const step_pin = 18;
-const direction_pin = 16;
-const ms1_pin = 36;
-const ms2_pin = 38;
-const ms3_pin = 40;
-
-//const motor = new MotorController(step_pin, direction_pin, ms1_pin, ms2_pin, ms3_pin);
-
-//motor.moveMotor(180, true, 1000);
-
-
-
-const Gpio = require('onoff').Gpio; // Gpio class
-const led = new Gpio(17, 'out');       // Export GPIO17 as an output
-let stopBlinking = false;
- 
-// Toggle the state of the LED connected to GPIO17 every 200ms
-const blinkLed = _ => {
-  if (stopBlinking) {
-    return led.unexport();
+function blinkLED() { //function to start blinking
+  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
   }
- 
-  led.read()
-    .then(value => led.write(value ^ 1))
-    .then(_ => setTimeout(blinkLed, 200))
-    .catch(err => console.log(err));
-};
- 
-blinkLed();
- 
-// Stop blinking the LED after 5 seconds
-setTimeout(_ => stopBlinking = true, 5000);
+}
+
+function endBlink() { //function to stop blinking
+  clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
+}
+
+setTimeout(endBlink, 5000); //stop blinking after 5 seconds
