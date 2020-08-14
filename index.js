1932df50521d39d3247f64b9e45ccd64e200f5c7
GageM <dgmille2@ncsu.edu>
2020-08-13 22:06:21 -0400

    onoff test

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { MotorCotroller } = require('./motorController');
-const gpio = require('rpi-gpio').promise;
 const MotorController = require('./motorController');
 
 const step_pin = 18;
@@ -14,12 +13,23 @@ const ms3_pin = 40;
 
 
 
-gpio.setup(7, gpio.DIR_OUT, write);
-
-function write(err) {
-    if (err) throw err;
-    gpio.write(7, false, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
-}
+const Gpio = require('../onoff').Gpio; // Gpio class
+const led = new Gpio(17, 'out');       // Export GPIO17 as an output
+let stopBlinking = false;
+ 
+// Toggle the state of the LED connected to GPIO17 every 200ms
+const blinkLed = _ => {
+  if (stopBlinking) {
+    return led.unexport();
+  }
+ 
+  led.read()
+    .then(value => led.write(value ^ 1))
+    .then(_ => setTimeout(blinkLed, 200))
+    .catch(err => console.log(err));
+};
+ 
+blinkLed();
+ 
+// Stop blinking the LED after 5 seconds
+setTimeout(_ => stopBlinking = true, 5000);
