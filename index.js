207fff2a85a6d83ba348ce85f9b6fd3688df47a2
GageM <dgmille2@ncsu.edu>
2020-10-27 00:23:13 -0400

    Add log

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ const m3_pin = 22;
 
 
 const motor = new MotorController(step_pin, direction_pin, m1_pin, m2_pin, m3_pin, true);
+const start = performance.now();
 motor.moveMotor(720, 1, 2500);
+const end = performance.now();
+
+console.log(`Time taken: ${end - start}`);
